54627bd54722e1673023128f69b9f1ad67a5d17c
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-10-06 13:50:19 -0500

    Update main.tf

--- examples/cp4i/main.tf
+++ examples/cp4i/main.tf
@@ -28,7 +28,8 @@ module "cp4i" {
   cluster_config_path = data.ibm_container_cluster_config.cluster_config.config_file_path
   storageclass        = var.storageclass
 
-  // Entitled Registry parameters:
+  // Cloud Pak parameters:
   entitled_registry_key        = var.entitled_registry_key
   entitled_registry_user_email = var.entitled_registry_user_email
+  namespace                    = local.namespace
 }
